6d7a214fdfc426e7300fbf59eb1ce25a632ae178
lyderic <lyderic@balzac.lyderic.net>
2026-01-19 07:21:30 +0100

    fixed quoting problem on info

--- scripts/info.lua
+++ scripts/info.lua
@@ -2,9 +2,12 @@ require "lclib"
 
 lines = { "Host,uproc,Distribution,Updates,Uptime,Avg" }
 for host, m in pairs(get_vigilax()) do
-	table.insert(lines, f([["%s","%s","%s","%s,%s,%s"]],
+	table.insert(lines, f([["%s","%s","%s","%s","%s","%s"]],
 		host, m.nproc,m.distro, m.updates,
 		m.uptime, m.loadavg))
 end
 
-x(f([[echo "%s" | xan view -pIM]], table.concat(lines, "\n")))
+output = table.concat(lines, "\n")
+xan = e("xan view -pIM","w")
+xan:write(output)
+xan:close()
